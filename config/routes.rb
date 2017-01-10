ac10265bed37fd8b80652041bba63e9bf419a7f1
Raghu Betina <raghu@firstdraft.com>
2017-01-10 02:08:17 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "foods#index"
   # Routes for the Cuisine resource:
   # CREATE
